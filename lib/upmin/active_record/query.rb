a6180b7595a1e7c6b346731781a131402b50b13e
Eric Bakan <eric@ebakan.com>
2014-09-29 05:58:50 -0400

    Removes distinct queries

--- lib/upmin/active_record/query.rb
+++ lib/upmin/active_record/query.rb
@@ -2,7 +2,7 @@ module Upmin::ActiveRecord
   module Query
 
     def results
-      return klass.model_class.ransack(search_options).result(distinct: true)
+      return klass.model_class.ransack(search_options).result
     end
 
     private
